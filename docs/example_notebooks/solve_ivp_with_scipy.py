1df4512f5364931babddff763d512dc8eda8311c
Nicholas Krämer <nicokraemer@gmx.net>
2022-09-23 11:56:07 +0200

    sync to py

--- docs/example_notebooks/solve_ivp_with_scipy.py
+++ docs/example_notebooks/solve_ivp_with_scipy.py
@@ -1,19 +1,19 @@
----
-jupyter:
-  jupytext:
-    formats: ipynb,md,py:percent
-    text_representation:
-      extension: .md
-      format_name: markdown
-      format_version: '1.3'
-      jupytext_version: 1.14.1
-  kernelspec:
-    display_name: Python 3 (ipykernel)
-    language: python
-    name: python3
----
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
-```python
+# %%
 import odezoo
 
 odezoo.set_backend("numpy")
@@ -23,18 +23,17 @@ import numpy as np
 import scipy.integrate
 
 import odezoo.ivps
-```
 
-```python
+# %%
 ivps = [
     odezoo.ivps.lotka_volterra(),
     odezoo.ivps.lotka_volterra(parameters=(1.0, 0.1, 0.1, 1.0)),
     odezoo.ivps.rigid_body(),
     odezoo.ivps.lorenz96(),
 ]
-```
 
-```python
+
+# %%
 def solve_ivp(ivp, **kwargs):
     def fun(_, y, *args):
         return ivp.vector_field(y, *args)
@@ -49,9 +48,9 @@ def solve_ivp(ivp, **kwargs):
 
     plotgrid = np.linspace(*t_span)
     return plotgrid, solution.sol(plotgrid).T
-```
 
-```python
+
+# %%
 fig, axes = plt.subplots(ncols=len(ivps), figsize=(8, 2), tight_layout=True)
 
 for ax, ivp in zip(axes, ivps):
@@ -59,8 +58,5 @@ for ax, ivp in zip(axes, ivps):
 
     ax.plot(xs, ys)
 plt.show()
-```
 
-```python
-
-```
+# %%
