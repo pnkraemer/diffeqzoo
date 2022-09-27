cd3dc31f31ab336112bc722a51dd0ddc1c70f545
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-09-27 15:56:00 +0200

    Tests pass

---
+++ odezoo/ivps.py
@@ -0,0 +1,257 @@
+"""Initial value problem examples.
+
+The following information should be available for each equation:
+
+- The equation (in a copy/pastable latex-math format)
+- The original reference
+- The meaning of each parameter (if possible)
+- "Notable" info (i.e. why this problem may be interesting)
+
+Providing this information makes it easy to use the problems
+as benchmark problems (e.g., in papers).
+"""
+
+
+from typing import Any, Callable, Iterable, NamedTuple, Union
+
+from odezoo import backend, vector_fields
+
+
+class _InitialValueProblem(NamedTuple):
+    vector_field: Callable
+    initial_values: Union[Iterable, Any]  # u0 or (u0, du0, ddu0, ...)
+    time_span: Iterable  # (t0, t1)
+    vector_field_args: Iterable = ()
+
+
+def lotka_volterra(*, initial_values=None, time_span=(0.0, 20.0), parameters=(0.5, 0.05, 0.5, 0.05)):
+    """Lotka--Volterra / predator-prey model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([20.0, 20.0])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.lotka_volterra,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def fitzhugh_nagumo(*, initial_values=None, time_span=(0.0, 20.0), parameters=(0.2, 0.2, 3.0, 1.0)):
+    r"""Construct the FitzHugh-Nagumo model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([1.0, -1.0])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.fitzhugh_nagumo,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def logistic(*, initial_values=None, time_span=(0.0, 2.5), parameters=(1.0, 1.0)):
+    """Logistic ODE model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([0.1])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.logistic,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def sir(*, initial_values=None, time_span=(0.0, 200.0), beta=0.3, gamma=0.1):
+    """SIR model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([998.0, 1.0, 1.0])
+
+    parameters = (beta, gamma, backend.numpy.sum(initial_values[0]))
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.sir,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def seir(*, initial_values=None, time_span=(0.0, 200.0), alpha=0.3, beta=0.3, gamma=0.1):
+    """SEIR model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([998.0, 1.0, 1.0, 1.0])
+
+    parameters = (alpha, beta, gamma, backend.numpy.sum(initial_values[0]))
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.seir,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def sird(*, initial_values=None, time_span=(0.0, 200.0), beta=0.3, gamma=0.1, eta=0.005):
+    """SIRD model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([998.0, 1.0, 1.0, 0.0])
+
+    parameters = (beta, gamma, eta, backend.numpy.sum(initial_values[0]))
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.sird,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def lorenz96(
+    *,
+    initial_values=None,
+    time_span=(0.0, 30.0),
+    num_variables=10,
+    forcing=8.0,
+    perturb=0.01,
+):
+    """Lorenz96 model."""
+    if initial_values is None:
+        initial_values = _lorenz96_chaotic_u0(forcing=forcing, num_variables=num_variables, perturb=perturb)
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.lorenz96,
+        vector_field_args=(forcing,),
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def lorenz63(
+    *,
+    initial_values=None,
+    time_span=(0.0, 20.0),
+    parameters=(10.0, 28.0, 8.0 / 3.0),
+):
+    """Lorenz63 model."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([0.0, 1.0, 1.05])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.lorenz63,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def rigid_body(*, time_span=(0.0, 20.0), initial_values=None, parameters=(-2.0, 1.25, -0.5)):
+    r"""Rigid body dynamics without external forces."""
+    if initial_values is None:
+        initial_values = backend.numpy.array([1.0, 0.0, 0.9])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.rigid_body,
+        vector_field_args=parameters,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def _lorenz96_chaotic_u0(*, forcing, num_variables, perturb):
+    u0_equilibrium = backend.numpy.ones(num_variables) * forcing
+    return backend.numpy.concatenate([backend.numpy.asarray([u0_equilibrium[0] + perturb]), u0_equilibrium[1:]])
+
+
+def pleiades(*, initial_values=None, time_span=(0.0, 3.0)):
+    """Construct the Pleiades problem in its original, second-order form."""
+    if initial_values is None:
+        x0 = [3.0, 3.0, -1.0, -3.0, 2.0, -2.0, 2.0]
+        y0 = [3.0, -3.0, 2.0, 0.0, 0.0, -4.0, 4.0]
+        dx0 = [0.0, 0.0, 0.0, 0.0, 0.0, 1.75, -1.5]
+        dy0 = [0.0, 0.0, 0.0, -1.25, 1.0, 0.0, 0.0]
+        u0 = backend.numpy.asarray(x0 + y0)
+        du0 = backend.numpy.asarray(dx0 + dy0)
+        initial_values = (u0, du0)
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.pleiades,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def pleiades_autonomous_api(**kwargs):
+    """Construct the Pleiades problem as \
+    :math:`\\ddot u(t) = f(u(t), \\dot u(t))` \
+    (with an unused second argument)."""  # noqa: D301
+    _, initial_values, time_span, args = pleiades(**kwargs)
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.pleiades_autonomous_api,
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def van_der_pol(*, stiffness_constant=1.0, initial_values=None, time_span=(0.0, 6.3)):
+    """Construct the Van-der-Pol system as a second-order differential equation."""
+    if initial_values is None:
+        u0 = backend.numpy.asarray([2.0])
+        du0 = backend.numpy.asarray([0.0])
+        initial_values = (u0, du0)
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.van_der_pol,
+        vector_field_args=(stiffness_constant,),
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def three_body(
+    *,
+    initial_values=None,
+    standardised_moon_mass=0.012277471,
+    time_span=(0.0, 17.0652165601579625588917206249),
+):
+    """Construct the restricted three-body problem as \
+    a second-order differential equation."""
+    if initial_values is None:
+        u0 = backend.numpy.asarray([0.994, 0])
+        du0 = backend.numpy.asarray([0, -2.00158510637908252240537862224])
+        initial_values = (u0, du0)
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.three_body,
+        vector_field_args=(standardised_moon_mass,),
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def hires(*, initial_values=None, time_span=(0.0, 321.8122)):
+    """Construct the High Irradiance Response (HIRES) problem."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([1.0, 0.0, 0.0, 0, 0, 0, 0, 0.0057])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.hires,
+        vector_field_args=(),  # todo: move vf-params here
+        initial_values=initial_values,
+        time_span=time_span,
+    )
+
+
+def rober(*, initial_values=None, time_span=(0.0, 1e5), k1=0.04, k2=3e7, k3=1e4):
+    """Construct the ROBER problem due to Robertson (1966)."""
+    if initial_values is None:
+        initial_values = backend.numpy.asarray([1.0, 0.0, 0.0])
+
+    return _InitialValueProblem(
+        vector_field=vector_fields.rober,
+        vector_field_args=(k1, k2, k3),
+        initial_values=initial_values,
+        time_span=time_span,
+    )
---
+++ odezoo/vector_fields.py
@@ -0,0 +1,124 @@
+"""ODE vector fields."""
+
+from odezoo import backend
+
+
+def lotka_volterra(y, /, a, b, c, d):
+    """Lotka--Volterra dynamics."""
+    return backend.numpy.asarray([a * y[0] - b * y[0] * y[1], -c * y[1] + d * y[0] * y[1]])
+
+
+def pleiades(u, /):
+    """Evaluate the Pleiades vector field in its original, second-order form."""
+    x, y = u[:7], u[7:]
+    x_diff = x[:, None] - x[None, :]
+    y_diff = y[:, None] - y[None, :]
+    r = (x_diff**2 + y_diff**2) ** 1.5
+
+    # We divide by r (elementwise) further below, but r contains zeros.
+    # Those zeros in r imply zeros in the nominator, so the following
+    # manipulation does not alter the result (but avoids warnings)
+    r = backend.numpy.where(r == 0, r + 1e-12, r)
+
+    mj = backend.numpy.arange(1, 8)[None, :]
+    ddx = backend.numpy.sum(mj * x_diff / r, axis=1)
+    ddy = backend.numpy.sum(mj * y_diff / r, axis=1)
+    return backend.numpy.concatenate((ddx, ddy))
+
+
+def pleiades_autonomous_api(u, _, /):
+    """Evaluate the Pleiades vector field as \
+    :math:`\\ddot u(t) = f(u(t), \\dot u(t))` \
+    (with an unused second argument)."""  # noqa: D301
+    return pleiades(u)
+
+
+def lorenz96(y, /, forcing):
+    """Lorenz96 dynamics."""
+    A = backend.numpy.roll(y, shift=-1)
+    B = backend.numpy.roll(y, shift=2)
+    C = backend.numpy.roll(y, shift=1)
+    D = y
+    return (A - B) * C - D + forcing
+
+
+def lorenz63(u, /, a, b, c):
+    """Lorenz63 dynamics."""
+    return backend.numpy.asarray([a * (u[1] - u[0]), u[0] * (b - u[2]) - u[1], u[0] * u[1] - c * u[2]])
+
+
+def rigid_body(y, /, p1, p2, p3):
+    r"""Rigid body dynamics without external forces."""
+    return backend.numpy.asarray([p1 * y[1] * y[2], p2 * y[0] * y[2], p3 * y[0] * y[1]])
+
+
+def logistic(u, p0, p1, /):
+    """Logistic ODE dynamics."""
+    return p0 * u * (1.0 - p1 * u)
+
+
+def fitzhugh_nagumo(u, /, a, b, c, d):
+    """FitzHugh--Nagumo model."""
+    return backend.numpy.asarray([u[0] - u[0] ** 3.0 / 3.0 - u[1] + a, (u[0] + b - c * u[1]) / d])
+
+
+def sir(u, /, beta, gamma, population_count):
+    """SIR model."""
+    du0_next = -beta * u[0] * u[1] / population_count
+    du1_next = beta * u[0] * u[1] / population_count - gamma * u[1]
+    du2_next = gamma * u[1]
+    return backend.numpy.asarray([du0_next, du1_next, du2_next])
+
+
+def seir(u, /, alpha, beta, gamma, population_count):
+    """SEIR model."""
+    du0_next = -beta * u[0] * u[2] / population_count
+    du1_next = beta * u[0] * u[2] / population_count - alpha * u[1]
+    du2_next = alpha * u[1] - gamma * u[2]
+    du3_next = gamma * u[2]
+    return backend.numpy.asarray([du0_next, du1_next, du2_next, du3_next])
+
+
+def sird(u, /, beta, gamma, eta, population_count):
+    """SIRD model."""
+    du0_next = -beta * u[0] * u[1] / population_count
+    du1_next = beta * u[0] * u[1] / population_count - gamma * u[1] - eta * u[1]
+    du2_next = gamma * u[1]
+    du3_next = eta * u[1]
+    return backend.numpy.asarray([du0_next, du1_next, du2_next, du3_next])
+
+
+def hires(u, /):  # todo: move parameters here
+    """High irradiance response."""
+    du1 = -1.71 * u[0] + 0.43 * u[1] + 8.32 * u[2] + 0.0007
+    du2 = 1.71 * u[0] - 8.75 * u[1]
+    du3 = -10.03 * u[2] + 0.43 * u[3] + 0.035 * u[4]
+    du4 = 8.32 * u[1] + 1.71 * u[2] - 1.12 * u[3]
+    du5 = -1.745 * u[4] + 0.43 * u[5] + 0.43 * u[6]
+    du6 = -280.0 * u[5] * u[7] + 0.69 * u[3] + 1.71 * u[4] - 0.43 * u[5] + 0.69 * u[6]
+    du7 = 280.0 * u[5] * u[7] - 1.81 * u[6]
+    du8 = -280.0 * u[5] * u[7] + 1.81 * u[6]
+    return backend.numpy.asarray([du1, du2, du3, du4, du5, du6, du7, du8])
+
+
+def rober(u, /, k1, k2, k3):
+    """Rober ODE problem."""
+    du0 = -k1 * u[0] + k3 * u[1] * u[2]
+    du1 = k1 * u[0] - k2 * u[1] ** 2 - k3 * u[1] * u[2]
+    du2 = k2 * u[1] ** 2
+    return backend.numpy.asarray([du0, du1, du2])
+
+
+def van_der_pol(u, du, /, stiffness_constant):
+    """Van-der-Pol dynamics as a second-order differential equation."""
+    return stiffness_constant * ((1.0 - u**2) * du - u)
+
+
+def three_body(Y, dY, /, standardised_moon_mass):
+    """Restricted three-body dynamics as a second-order differential equation."""
+    mu, mp = standardised_moon_mass, 1.0 - standardised_moon_mass
+    D1 = backend.numpy.linalg.norm(backend.numpy.asarray([Y[0] + mu, Y[1]])) ** 3.0
+    D2 = backend.numpy.linalg.norm(backend.numpy.asarray([Y[0] - mp, Y[1]])) ** 3.0
+    du0p = Y[0] + 2 * dY[1] - mp * (Y[0] + mu) / D1 - mu * (Y[0] - mp) / D2
+    du1p = Y[1] - 2 * dY[0] - mp * Y[1] / D1 - mu * Y[1] / D2
+    return backend.numpy.asarray([du0p, du1p])
